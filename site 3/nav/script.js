a31761b513bab9adb3ccf775b5e12c836caff629
Filip Trajceski <153946352+FilipTrajceski@users.noreply.github.com>
2024-07-08 16:27:58 +0200

    Delete site 3 directory

--- site 3/nav/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var toggler = document.querySelector(".navbar-toggler");
-  var navbarCollapse = document.querySelector("#navbarNav");
-
-  toggler.addEventListener("click", function () {
-    navbarCollapse.classList.toggle("show");
-  });
-});
